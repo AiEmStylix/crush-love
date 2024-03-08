013165ea4c8eff4e1c4f732a4c52babb127a5014
Vũ Bình Dương <78728283+AiEmStylix@users.noreply.github.com>
2024-03-08 14:00:22 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "Chào cậu, tớ có điều muốn nói. </br> 8/3 vui vẻ nhé!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
+    "Hãy nhận lời chúc 8/3 của tớ nhé.",
+  buttonYes: "OK",
+  buttonNo: "Đéo",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
-    "Chúc cô nàng một ngày 8/3 giàu năng lượng, được nhiều hoa nhé. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br>",
+    "Chúc cô nàng một ngày 8/3 giàu năng lượng, được nhiều hoa nhé. </br> Have a good day. </br>",
 };
